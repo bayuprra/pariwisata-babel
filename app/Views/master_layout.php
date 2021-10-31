b4e9a222c0266e74e98973496fa2deeedb2594b7
Tasya Dwi A. S <tasyadwiaskarasiahaan@gmail.com>
2021-10-31 22:29:27 +0700

    finishing main menu

--- app/Views/master_layout.php
+++ app/Views/master_layout.php
@@ -7,19 +7,44 @@
     <title>Example</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url()?>/main/master.css">
     <?= $this->renderSection('style') ?>
     <script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js"
             integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="container">
+<div class="container col-12">
     <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand">Traveloha</a>
-        <form class="form-inline" id="formItem">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search"  id="keyword" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="searchItem">Search</button>
-        </form>
-        <button class="btn btn-primary" id="cart"><i class="fas fa-shopping-cart"></i>(0)</button>
+
+        <li><a class="nav-link active" href="#">Home</a></li>
+        <li><a class="nav-link" href="#">Berita</a></li>
+        <li><a class="nav-link" href="#">Tempat Wisata</a></li>
+        <li><a class="nav-link" href="#">Partner</a></li>
+
+        <!-- Logout -->
+        <div class="logout">
+            <div class="dropdown">
+                <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user-circle fa-2x"></i>
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right">
+                    <!-- condition if the user is admin-->
+<!--                        <a class="dropdown-item" data-toggle="modal" data-target="#profileAdmin" data-backdrop="static" data-keyboard="false">Profile</a>-->
+                    <!-- end-->
+                    <!-- condition if the user is user-->
+<!--                        <a class="dropdown-item" data-toggle="modal" data-target="#profileUser" data-backdrop="static" data-keyboard="false">Profile</a>-->
+                    <!-- end-->
+                    <a class="dropdown-item" data-toggle="modal" data-target="#exampleModal" data-backdrop="static" data-keyboard="false">Reset Password</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('#').submit();">Logout
+                        <form id="logout-form" action="#" method="POST" style="display: none;">
+                        </form>
+                    </a>
+                </div>
+            </div>
+        </div>
     </nav>
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
@@ -29,6 +54,12 @@
 </div>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>
+    $(".nav a").on("click", function(){
+        $(".nav").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+    });
+</script>
 <?= $this->renderSection('script') ?>
 </body>
 </html>
--- app/Views/users/home.php
+++ app/Views/users/home.php
@@ -1,9 +1,55 @@
 <?= $this->extend('master_layout') ?>
 
-<?= $this->section('content') ?>
-<div class="row">
-    <div class="row col-md-12 mt-2"  id="listBarang" >
+<?= $this->section('style') ?>
+    <!-- recomendation's style -->
+    <link rel="stylesheet" href="<?php echo base_url()?>/main/main.css">
+<?= $this->endSection() ?>
 
+<?= $this->section('content') ?>
+<div class="wrapper row">
+    <div class="col-md-6 mt-2"></div>
+    <div class="col-md-6 mt-2">
+        <form class="form-inline" id="formItem">
+            <input class="form-control" type="search" placeholder="Search" id="keyword" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="searchItem">Search</button>
+        </form>
+    </div>
+</div>
+
+<div class="wrapper row">
+    <div class="col-md-6 mt-2">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="https://asset.kompas.com/crops/A-10Y09CvBgLvsZ7i4FtOiJSdNA=/2x151:1079x869/750x500/data/photo/2020/11/11/5fabf60a4db83.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://img.okezone.com/content/2019/06/06/406/2064174/5-tempat-wisata-di-bandung-yang-cocok-untuk-liburan-lebaran-jRc1eFQ2QO.png" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://asset.kompas.com/crops/dbjCKOaKpWze7jbMRFHwTZTMXsQ=/0x0:1080x720/750x500/data/photo/2020/11/11/5fabf25c3111b.jpg" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <p></p>
+        <h4><pre>       Temukan Tempat Wisata yang Ingin
+
+       Kamu Kunjungi di Bangka Belitung</pre></h4>
+    </div>
+    <div class="row col-md-6 mt-2 bg-light main"  id="listBarang">
     </div>
 </div>
 <?= $this->endSection() ?>
@@ -21,18 +67,19 @@
     var tampungbarang = document.getElementById('listBarang');
 
     function printItems(array) {
-        var tampung = "";
+        var tampung = '';
+
         for(var i=0; i <array.length; i++){
-            var databarang = array[i]
+            var tempatWisata = array[i]
             tampung += `
             <!-- Component Card dari Bootstrap -->
             <div class ="col-4 mt-2">
-            <div class="card" style="width: 18rem;">
-            <img src="${databarang[3]}" class="card-img-top" height="200px" width="200px" alt="...">
+            <div class="card" style="width: 12rem;">
+            <img src="${tempatWisata[3]}" class="card-img-top" height="120px" width="120px" alt="...">
             <div class="card-body">
-            <h5 class="card-title" id="itemDesc">${databarang[2]}</h5>
-            <p class="card-text" id="itemName">${databarang[1]}</p>
-            <a href="#" class="btn btn-primary" id="addCart" onClick="addCart()">Add To Cart</a>
+            <h5 class="card-title" id="itemDesc">${tempatWisata[2]}</h5>
+            <p class="card-text" id="itemName">${tempatWisata[1]}</p>
+            <a href="#" class="btn btn-primary">Detail</a>
             </div>
             </div>
             </div>`
@@ -63,11 +110,5 @@
         printItems(terfilter)
     })
 
-    var cart = document.getElementById("cart")
-    var carNumber = cart.value
-    function addCart() {
-        carNumber++;
-        cart.innerHTML = `<i class="fas fa-shopping-cart"></i>(${carNumber})`
-    }
 </script>
 <?= $this->endSection() ?>
